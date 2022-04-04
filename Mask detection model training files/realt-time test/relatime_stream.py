fda4c8271f58d2cb27398cbc8e82411a4eab2e75
Kolitha Senevirathne <kolithasenevirathne@gmail.com>
2022-04-04 14:16:26 +0530

    created real time stream program for face mask detection

---
+++ Mask detection model training files/realt-time test/relatime_stream.py
@@ -0,0 +1,127 @@
+
+import cv2
+import time
+import argparse
+
+import numpy as np
+from PIL import Image
+from anchor_generation import anchor_generate
+from anchor_decoding import bboxdecode
+from nms_function import non_maximum_suppression
+from tf_model_loder import model_load, tf_inference
+
+sess, graph = model_load('models/savedmodel.pb')
+feature_map_sizes = [[33, 33], [17, 17], [9, 9], [5, 5], [3, 3]]
+anchor_sizes = [[0.04, 0.056], [0.08, 0.11], [0.16, 0.22], [0.32, 0.45], [0.64, 0.72]]
+anchor_ratios = [[1, 0.62, 0.42]] * 5
+
+
+anchors = anchor_generate(feature_map_sizes, anchor_sizes, anchor_ratios)
+
+
+anchors_exp = np.expand_dims(anchors, axis=0)
+
+id2class = {0: 'With Mask', 1: 'Without Mask'}
+
+
+def inference(image,
+              conf_thresh=0.5,
+              iou_thresh=0.4,
+              target_shape=(160, 160),
+              draw_result=True,
+              show_result=True
+              ):
+ 
+    output_info = []
+    height, width, _ = image.shape
+    image_resized = cv2.resize(image, target_shape)
+    image_np = image_resized / 255.0
+    image_exp = np.expand_dims(image_np, axis=0)
+    y_bboxes_output, y_cls_output = tf_inference(sess, graph, image_exp)
+
+
+    y_bboxes = bboxdecode(anchors_exp, y_bboxes_output)[0]
+    y_cls = y_cls_output[0]
+
+    bbox_max_scores = np.max(y_cls, axis=1)
+    bbox_max_score_classes = np.argmax(y_cls, axis=1)
+
+    keep_idxs = non_maximum_suppression(y_bboxes,
+                                                 bbox_max_scores,
+                                                 conf_thresh=conf_thresh,
+                                                 iou_thresh=iou_thresh,
+                                                 )
+
+    for idx in keep_idxs:
+        conf = float(bbox_max_scores[idx])
+        class_id = bbox_max_score_classes[idx]
+        bbox = y_bboxes[idx]
+
+        xmin = max(0, int(bbox[0] * width))
+        ymin = max(0, int(bbox[1] * height))
+        xmax = min(int(bbox[2] * width), width)
+        ymax = min(int(bbox[3] * height), height)
+
+        if draw_result:
+            if class_id == 0:
+                color = (0, 255, 0)
+            else:
+                color = (255, 0, 0)
+            cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
+            cv2.putText(image, "%s: %.2f" % (id2class[class_id], conf), (xmin + 2, ymin - 2),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.8, color)
+        output_info.append([class_id, conf, xmin, ymin, xmax, ymax])
+
+    if show_result:
+        Image.fromarray(image).show()
+    return output_info
+
+
+def stream(video_path, output_video_name, conf_thresh):
+    cap = cv2.VideoCapture(video_path)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+    total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    if not cap.isOpened():
+        raise ValueError("Video as bee failed to open.")
+        return
+    status = True
+    idx = 0
+    while status:
+        start_stamp = time.time()
+        status, img_raw = cap.read()
+        img_raw = cv2.cvtColor(img_raw, cv2.COLOR_BGR2RGB)
+        read_frame_stamp = time.time()
+        if (status):
+            inference(img_raw,
+                      conf_thresh,
+                      iou_thresh=0.5,
+                      target_shape=(260, 260),
+                      draw_result=True,
+                      show_result=False)
+            cv2.imshow('image', img_raw[:, :, ::-1])
+            cv2.waitKey(1)
+        
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Dtect face masks on a stream")
+    parser.add_argument('--img-mode', type=int, default=1, help='1 for image, 0 for video.')
+    parser.add_argument('--img-path', type=str, help='path to image.')
+    parser.add_argument('--video-path', type=str, default='0', help='path to video, 0 for camera.')
+
+    args = parser.parse_args()
+    if args.img_mode:
+        imgPath = args.img_path
+        img = cv2.imread(imgPath)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        inference(img, show_result=True, target_shape=(260, 260))
+    else:
+        video_path = args.video_path
+        if args.video_path == '0':
+            video_path = 0
+        stream(video_path, '', conf_thresh=0.5)
